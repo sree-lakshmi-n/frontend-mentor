30e1cc7b71d73bc8fc7d8b0eff1d66e3cfcd347b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-08-25 15:18:06 +0530

    function _ to get first element of given classname

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -1,5 +1,9 @@
 `use strict`;
+const _ = (e) => {
+  return document.getElementsByClassName(e)[0];
+};
 const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 days.forEach((day) => {
   console.log(day);
 });
+console.log((_("spending-weekly-metrics").innerText = "Hi"));
