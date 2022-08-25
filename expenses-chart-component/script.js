f4a9661eb6d1137cce666fdef3128a84d12de57d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-08-25 15:27:53 +0530

    Added container divs for all days

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -2,9 +2,11 @@
 const _ = (e) => {
   return document.getElementsByClassName(e)[0];
 };
+const week = _("spending-weekly-metrics");
+
 const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 days.forEach((day) => {
-  console.log(day);
+  const dayContainer = document.createElement("div");
+  dayContainer.className = `${day}-container`;
+  week.appendChild(dayContainer);
 });
-
-const week = _("spending-weekly-metrics");
