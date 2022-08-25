5cb98f1fb661b98b8dcdc811a91d34045867c79d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-08-25 15:57:17 +0530

    Created graph divs and labels for each day

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -1,12 +1,24 @@
 `use strict`;
 const _ = (e) => {
-  return document.getElementsByClassName(e)[0];
+  return document.getElementsByClassName(e);
 };
 const week = _("spending-weekly-metrics");
 
 const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 days.forEach((day) => {
   const dayContainer = document.createElement("div");
-  dayContainer.className = `${day}-container`;
-  week.appendChild(dayContainer);
+
+  dayContainer.className = `day-container ${day}-container`;
+  week[0].appendChild(dayContainer);
+});
+
+const dayContainers = _(`day-container`);
+Array.from(dayContainers).forEach((dayContainer, index) => {
+  const dayGraph = document.createElement("div");
+  const dayLabel = document.createElement("span");
+  dayGraph.className = `day-graph ${days[index]}-graph`;
+  dayLabel.className = `day-label ${days[index]}-label`;
+  dayLabel.textContent = days[index];
+  dayContainer.appendChild(dayGraph);
+  dayContainer.appendChild(dayLabel);
 });
