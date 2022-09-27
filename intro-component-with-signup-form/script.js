5774af275e5c699180a14609aa2022db2a81b175
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-27 17:08:59 +0530

    adding event listener to submit button

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -3,3 +3,7 @@ const firstName = document.getElementsByClassName("first-name")[0];
 const lastName = document.getElementsByClassName("last-name")[0];
 const mailId = document.getElementsByClassName("mail-id")[0];
 const passWord = document.getElementsByClassName("pwd")[0];
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+});
