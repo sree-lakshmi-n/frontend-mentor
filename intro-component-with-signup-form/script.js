3f3d91f0dd08e96b86aa0dc13d77a60a9364a7a4
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-27 17:30:20 +0530

    fetched submit btn, added event listener to it.

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -1,9 +1,10 @@
 `use strict`;
-const firstName = document.getElementsByClassName("first-name")[0];
+const firstName = document.querySelector(".first-name");
 const lastName = document.getElementsByClassName("last-name")[0];
 const mailId = document.getElementsByClassName("mail-id")[0];
 const passWord = document.getElementsByClassName("pwd")[0];
+const submitBtn = document.getElementsByClassName("submit-btn")[0];
 
-document.addEventListener("click", (e) => {
-  console.log(e.target);
+submitBtn.addEventListener("click", (e) => {
+  console.log(firstName.value, lastName.value, mailId.value, passWord.value);
 });
