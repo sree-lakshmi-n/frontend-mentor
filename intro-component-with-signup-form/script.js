d51479995d7211015327d802cdae83bd4e91a845
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-27 18:43:59 +0530

    validateInputs function

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -5,20 +5,36 @@ const mailId = document.getElementsByClassName("mail-id")[0];
 const passWord = document.getElementsByClassName("pwd")[0];
 const submitBtn = document.getElementsByClassName("submit-btn")[0];
 
-submitBtn.addEventListener("click", (e) => {});
-
 // Checking if input values are empty
 const isEmpty = (str) => {
   return !str.trim().length;
 };
 
-const inputs = [firstName, lastName, mailId, passWord];
-inputs.forEach((e) => {
-  if (isEmpty(e.value)) {
-    e.parentElement.setAttribute(
-      "data-error",
-      `${e.placeholder} cannot be empty`
-    );
-  }
-  console.log(e.parentElement.getAttribute("data-error"));
-});
+// const inputs = [firstName, lastName, mailId, passWord];
+// inputs.forEach((e) => {
+//   if (isEmpty(e.value)) {
+//     e.parentElement.setAttribute(
+//       "data-error",
+//       `${e.placeholder} cannot be empty`
+//     );
+//   }
+//   console.log(e.parentElement.getAttribute("data-error"));
+// });
+
+const regName = /^[a-zA-Z ]{2,30}$/g;
+const regMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+const minPwdLen = 8;
+
+const validateInputs = (arg, type) => {
+  const res = "";
+  if (isEmpty(arg)) res = "cannot be empty";
+  else if (
+    (type === "name" && regName.test(arg)) ||
+    (type === "mail" && regMail.test(arg)) ||
+    type === "pwd"
+  )
+    res = "";
+  else res = "is not valid";
+};
+
+submitBtn.addEventListener("click", (e) => {});
