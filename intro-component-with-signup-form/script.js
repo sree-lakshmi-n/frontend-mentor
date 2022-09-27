518752dee907411c9ac8f9a85425e9d8d6104895
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-27 18:03:35 +0530

    Checking if input values are empty

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -5,6 +5,20 @@ const mailId = document.getElementsByClassName("mail-id")[0];
 const passWord = document.getElementsByClassName("pwd")[0];
 const submitBtn = document.getElementsByClassName("submit-btn")[0];
 
-submitBtn.addEventListener("click", (e) => {
-  console.log(firstName.value, lastName.value, mailId.value, passWord.value);
+submitBtn.addEventListener("click", (e) => {});
+
+// Checking if input values are empty
+const isEmpty = (str) => {
+  return !str.trim().length;
+};
+
+const inputs = [firstName, lastName, mailId, passWord];
+inputs.forEach((e) => {
+  if (isEmpty(e.value)) {
+    e.parentElement.setAttribute(
+      "data-error",
+      `${e.placeholder} cannot be empty`
+    );
+  }
+  console.log(e.parentElement.getAttribute("data-error"));
 });
