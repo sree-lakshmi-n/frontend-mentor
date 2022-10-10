43072ec99cbba7e1685936bb3108882601080d0d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-10 18:04:59 +0530

    dark theme toggling for body and to do section items

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,9 +2,14 @@
 const themeToggleBtn = document.getElementsByClassName(
   "header__toggle-button"
 )[0];
+const todoItems = document.getElementsByClassName("section-to-do__items");
 
 const themeToggle = () => {
   themeToggleBtn.classList.toggle("header-toggle-button-dark");
+  document.body.classList.toggle("body-bg-dark");
+  Array.from(todoItems).forEach((e) => {
+    e.classList.toggle("section-to-do__items-dark");
+  });
 };
 
 themeToggleBtn.addEventListener("click", themeToggle);
