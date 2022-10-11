b9750aaa207211898a07756fe08dad00df4b54d1
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-11 08:47:50 +0530

    dark theme toggling

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,14 +2,41 @@
 const themeToggleBtn = document.getElementsByClassName(
   "header__toggle-button"
 )[0];
+const todoSectionList = document.getElementsByClassName(
+  "section-to-do__list"
+)[0];
 const todoItems = document.getElementsByClassName("section-to-do__items");
+const todoTextBox = document.getElementsByClassName(
+  "section-to-do__items-textbox"
+);
+const todoCheckBox = document.getElementsByClassName("btn-checkbox");
+const textCompleted = document.getElementsByClassName("textbox-completed");
+const todoMetrics = document.getElementsByClassName(
+  "section-to-do__metrics"
+)[0];
+const todoInstructions = document.getElementsByClassName(
+  "section-to-do__instructions"
+)[0];
 
 const themeToggle = () => {
   themeToggleBtn.classList.toggle("header-toggle-button-dark");
+
   document.body.classList.toggle("body-bg-dark");
+  todoSectionList.classList.toggle("section-to-do__list-dark");
   Array.from(todoItems).forEach((e) => {
     e.classList.toggle("section-to-do__items-dark");
   });
+  Array.from(todoTextBox).forEach((e) => {
+    e.classList.toggle("section-to-do__items-textbox-dark");
+  });
+  Array.from(todoCheckBox).forEach((e) => {
+    e.classList.toggle("btn-checkbox-dark");
+  });
+  Array.from(textCompleted).forEach((e) => {
+    e.classList.toggle("textbox-completed-dark");
+  });
+  todoMetrics.classList.toggle("section-to-do__metrics-dark");
+  todoInstructions.classList.toggle("section-to-do__instructions-dark");
 };
 
 themeToggleBtn.addEventListener("click", themeToggle);
