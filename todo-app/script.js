46a27b71e4138ac7f62d06ec1b70b25499b8855b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-12 10:30:46 +0530

    todoMetricsList toggle theme

--- todo-app/script.js
+++ todo-app/script.js
@@ -13,7 +13,7 @@ const btnCheckBox = _("btn-checkbox");
 const btnClose = _("btn-close");
 const textCompleted = _("textbox-completed");
 const todoMetrics = _("section-to-do__metrics")[0];
-const todoMetricsList = _("section-to-do__metrics-nav-list")[0];
+const todoMetricsList = _("section-to-do__metrics-nav-list");
 const todoClearBtn = _("section-to-do__metrics-clear-btn")[0];
 const todoInstructions = _("section-to-do__instructions")[0];
 
@@ -32,11 +32,11 @@ const themeToggle = () => {
   toggleElementTheme(document.body, "body-bg-dark");
   toggleElementTheme(todoSectionList, "section-to-do__list-dark");
   toggleElementTheme(todoMetrics, "section-to-do__metrics-dark");
-  toggleElementTheme(todoMetricsList, "section-to-do__metrics-nav-list-dark");
   toggleElementTheme(todoClearBtn, "section-to-do__metrics-clear-btn-dark");
   toggleElementTheme(todoInstructions, "section-to-do__instructions-dark");
 
   toggleListTheme(todoItems, "section-to-do__items-dark");
+  toggleListTheme(todoMetricsList, "section-to-do__metrics-nav-list-dark");
   toggleListTheme(todoTextBox, "section-to-do__items-textbox-dark");
   toggleListTheme(btnCheckBox, "btn-checkbox-dark");
   toggleListTheme(btnClose, "btn-close-dark");
