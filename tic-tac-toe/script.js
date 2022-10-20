3d495484fc85096a41e19ec28048be890f1bd551
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-20 14:58:25 +0530

    Making of grid cells

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,8 +3,15 @@ const _ = (className) => {
 };
 
 const noOfGridCells = 9;
+let currentPlayer = "X";
+let players = [];
+
+const playerTurn = _("player-turn")[0];
+const gameChoices = _("game-choices-marks")[0];
+const gameCompetitorChoices = _("game-competitor-choices")[0];
 const playGrid = _("play-grid")[0];
 
+// Making of grid cells
 const makeGridCells = () => {
   for (let i = 0; i < noOfGridCells; i++) {
     const gridCell = document.createElement("div");
@@ -14,12 +21,48 @@ const makeGridCells = () => {
   }
 };
 
-addEventListener("DOMContentLoaded", makeGridCells);
+addEventListener("DOMContentLoaded", () => {
+  makeGridCells();
+  gameSetup();
+});
 
 // Game Logic
-const currentPlayer = "X";
-const playerTurn = _("player-turn")[0];
 
 const gameSetup = () => {
   playerTurn.textContent = currentPlayer;
+  gameMarkChoice();
+};
+
+const markSelected = (mark) => {
+  mark.classList.add("selected");
+  player1 = mark.textContent;
+};
+
+const removeSelected = () => {
+  Array.from(gameChoices.children).forEach((mark) => {
+    mark.classList.remove("selected");
+  });
+};
+
+const gameMarkChoice = () => {
+  Array.from(gameChoices.children).forEach((mark) => {
+    mark.addEventListener("click", () => {
+      removeSelected(gameChoices);
+      markSelected(mark);
+    });
+  });
+};
+
+// Game Competitor Choice
+
+const playVsCPU = () => {
+  gameType = "CPU";
+};
+
+const gameCompetitorChoice = () => {
+  _("game-competitor-choices-cpu")[0].addEventListener("click", playVsCPU);
+  _("game-competitor-choices-player")[0].addEventListener(
+    "click",
+    playVsPlayer
+  );
 };
