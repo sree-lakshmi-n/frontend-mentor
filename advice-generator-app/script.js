63d95b4be63416b4904a464d178c9f221e8aa2a9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-14 18:14:34 +0530

    api fetched and advice id and text added to document

---
+++ advice-generator-app/script.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+const adviceId = document.getElementsByClassName("advice-id")[0];
+const adviceText = document.getElementsByClassName("advice-text")[0];
+
+async function getQuote() {
+  let response = await fetch("https://api.adviceslip.com/advice");
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
+
+getQuote().then((data) => {
+  let {
+    slip: { id, advice },
+  } = data;
+  adviceId.textContent = id;
+  adviceText.textContent = advice;
+});
