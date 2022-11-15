001c63b09c05bb9da75d64be1b3191b9646cc242
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-15 13:07:01 +0530

    quote regenerated on clicking dice

--- advice-generator-app/script.js
+++ advice-generator-app/script.js
@@ -1,19 +1,23 @@
 `use strict`;
 
+const container = document.getElementsByClassName("container")[0];
 const adviceId = document.getElementsByClassName("advice-id")[0];
 const adviceText = document.getElementsByClassName("advice-text")[0];
 
 async function getQuote() {
   let response = await fetch("https://api.adviceslip.com/advice");
   let data = await response.json();
-  console.log(data);
   return data;
 }
 
-getQuote().then((data) => {
-  let {
-    slip: { id, advice },
-  } = data;
-  adviceId.textContent = id;
-  adviceText.textContent = `"${advice}"`;
-});
+const changeQuote = () => {
+  getQuote().then((data) => {
+    let {
+      slip: { id, advice },
+    } = data;
+    adviceId.textContent = id;
+    adviceText.textContent = `"${advice}"`;
+  });
+};
+changeQuote();
+container.addEventListener("click", changeQuote);
