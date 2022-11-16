1e65d38756a6d62c30b8cd941ee8a42d18352db3
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-16 17:50:50 +0530

    getting ip details and displaying it in ip-details section

--- ip-address-tracker/script.js
+++ ip-address-tracker/script.js
@@ -1,1 +1,35 @@
 `use strict`;
+
+async function getIPDetails(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+function displayIPDetails() {
+  const ipAddress =
+    document.getElementsByClassName("search-bar-input")[0].value;
+  const url = `https://geo.ipify.org/api/v2/country?apiKey=at_vMIjof1g6XVqaF1luAuKpjEHZwIvf&ipAddress=${ipAddress}`;
+
+  getIPDetails(url).then((data) => {
+    console.log(data);
+    data.ip ? addIPDetailsContent(data) : alert("Enter valid IP address");
+  });
+}
+
+const addIPDetailsContent = (data) => {
+  const {
+    ip,
+    location: { country, region, timezone },
+    isp,
+  } = data;
+  console.log(ip, country, region, timezone, isp);
+  document.querySelector(".ip-address .ip-details-content").textContent = ip;
+  document.querySelector(
+    ".ip-location .ip-details-content"
+  ).textContent = `${region}, ${country}`;
+  document.querySelector(
+    ".ip-timezone .ip-details-content"
+  ).textContent = `UTC ${timezone}`;
+  document.querySelector(".ip-isp .ip-details-content").textContent = isp;
+};
